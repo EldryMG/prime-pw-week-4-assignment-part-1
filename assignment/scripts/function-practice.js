6a2023f919530a8823515762258953ca2a1eb71e
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-08-04 18:19:38 -0500

    Completed assignment 4 part 1. No stretch goals.

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,59 +15,66 @@ console.log(hello());
 
 // 2. Function to return a personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
+function helloName(name) {
   return `Hello, ${name} !`;
 }
 // Remember to call the function to test
-console.log (helloName(`Jo`));
+console.log(helloName(`Jo`));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers(firstNumber,secondNumber ) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
   return firstNumber + secondNumber;
 }
-console.log(addNumbers(3,4));
+console.log(addNumbers(3, 4));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree(num1,num2,num3){
-  return num1*num2*num3
+function multiplyThree(num1, num2, num3) {
+  return num1 * num2 * num3
 }
 
-console.log(multiplyThree(3,3,2));
+console.log(multiplyThree(3, 3, 2));
 
 // 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ){
+function isPositive(number) {
+  if (number > 0) {
     return true;
   }
-    return false;
+  return false;
 }
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log('isPositive - should say true', isPositive(3));
+console.log('isPositive - should say false', isPositive(0));
+console.log('isPositive - should say false', isPositive(-3));
 
 
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
-qSix = ['22', '4309', '6353', '83811297'];
+qSix = ['22', '4309', '635', '838', '6', '64'];
 
-function getLast( array ) {
+function getLast(array) {
   return array.pop();
-  if (array.length === 0){
+  if (array.length === 0) {
     return;
   }
 }
 console.log(getLast(qSix));
-
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
-function find( value, array ){
-
+function find(value, array) {
+  for (let i = 0; i < array.length; i++) {
+    if (value === array[i]) {
+      return true;
+    }
+  }
+  return false;
 }
+
+console.log(find('6', qSix));
+
 
 // ----------------------
 // Stretch Goals
@@ -77,11 +84,11 @@ function find( value, array ){
 function isFirstLetter(letter, string) {
 
 }
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
+console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll() {
   let sum = 0
   // TODO: loop to add items
   return sum;
@@ -95,4 +102,4 @@ function sumAll( ) {
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or
 //     CodeWars(https://www.codewars.com/). Then describe it
-//     here in a comment, write the function, and test it!
+//     here in a comment, write the function, and test it
